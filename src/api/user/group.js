736cccb1424930646d0bdc0b3ad9acb4d55d4117
ruil1n <ruilin911@outlook.com>
2018-07-22 18:33:04 +0800

    fix

--- src/api/user/group.js
+++ src/api/user/group.js
@@ -6,7 +6,7 @@ import request from '@/utils/fetch'
  * @param {Number} page_size 页数据量
  */
 export function getAllGroupsByPage(cur_page,page_size) {
-    return request.get('/user/getAllGroupByPage/0/10'+cur_page+'/'+page_size)
+    return request.get('/user/getAllGroupByPage/'+cur_page+'/'+page_size)
 }
 
 /**
